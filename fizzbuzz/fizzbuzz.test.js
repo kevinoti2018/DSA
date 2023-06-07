0187a9357e2969dc10937ff283bf04cdf35e73d0
kevin otieno <kelvinotix@gmail.com>
2023-06-07 15:15:04 +0300

    added performance and ci cd

--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -4,33 +4,33 @@ describe('fizzBuzz',()=>{
     test("FizzBuzz should return an empty array for negative numbers", () => {
         expect(fizzBuzz(-10)).toEqual([]);
       });
-    test(" should return the correct sequence up to 15", () => {
-        expect(fizzBuzz(15)).toEqual([
-          1,
-          2,
-          "Fizz",
-          4,
-          "Buzz",
-          "Fizz",
-          7,
-          8,
-          "Fizz",
-          "Buzz",
-          11,
-          "Fizz",
-          13,
-          14,
-          "FizzBuzz",
-        ]);
-      });
+    // test(" should return the correct sequence up to 15", () => {
+    //     expect(fizzBuzz(15)).toEqual([
+    //       1,
+    //       2,
+    //       "Fizz",
+    //       4,
+    //       "Buzz",
+    //       "Fizz",
+    //       7,
+    //       8,
+    //       "Fizz",
+    //       "Buzz",
+    //       11,
+    //       "Fizz",
+    //       13,
+    //       14,
+    //       "FizzBuzz",
+    //     ]);
+    //   });
       test("return an empty array for 0", () => {
         expect(fizzBuzz(0)).toEqual([]);
       });
       test('should return 1', () => {
         expect(fizzBuzz(1)).toEqual([1]);
       });
-      test('should return the correct sequence up to 5', () => {
-        expect(fizzBuzz(5)).toEqual([1, 2, 'Fizz', 4, 'Buzz']);
-      });
+      // test('should return the correct sequence up to 5', () => {
+      //   expect(fizzBuzz(5)).toEqual([1, 2, 'Fizz', 4, 'Buzz']);
+      // });
       
 })
--- index.js
+++ index.js
@@ -124,30 +124,31 @@
 // let value = "1118888";
 // let mostAppearingChar = frequentCharacter(value);
 // console.log(mostAppearingChar);
-function pattern(num) {
-  const n = Math.abs(num);
-  const patternArray = [];
 
-  for (let i = 0; i < n; i++) {
-    let stair = '';
-    for (let j = 0; j < n; j++) {
-      if (j <= i) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    patternArray.push(stair);
-  }
+// function pattern(num) {
+//   const n = Math.abs(num);
+//   const patternArray = [];
 
-  return patternArray;
-}
+//   for (let i = 0; i < n; i++) {
+//     let stair = '';
+//     for (let j = 0; j < n; j++) {
+//       if (j <= i) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     patternArray.push(stair);
+//   }
 
-module.exports = pattern;
+//   return patternArray;
+// }
 
-const num = 5;
-const result = pattern(num);
-console.log(result);
+// module.exports = pattern;
+
+// const num = 5;
+// const result = pattern(num);
+// console.log(result);
 
 
 
@@ -198,3 +199,272 @@ console.log(result);
 //   steps(n, row, stair);
 // }
 // steps(9)
+
+
+
+// function generateMatrix(rows, columns) {
+//   const matrix = [];
+//   for (let i = 0; i < rows; i++) {
+//     matrix[i] = [];
+//     for (let j = 0; j < columns; j++) {
+//       matrix[i][j] = 0; // Initialize all cells with 0
+//     }
+//   }
+
+//   let value = 1;
+//   let increment = 1;
+//   let row = 0;
+//   let col = 0;
+
+//   while (value <= rows * columns) {
+//     // Traverse right
+//     for (let i = col; i < columns - col; i++) {
+//       matrix[row][i] = value;
+//       value++;
+//     }
+//     // Traverse down
+//     for (let i = row + 1; i < rows - row; i++) {
+//       matrix[i][columns - col - 1] = value;
+//       value++;
+//     }
+//     // Traverse left
+//     for (let i = columns - col - 2; i >= col; i--) {
+//       matrix[rows - row - 1][i] = value;
+//       value++;
+//     }
+//     // Traverse up
+//     for (let i = rows - row - 2; i > row; i--) {
+//       matrix[i][col] = value;
+//       value++;
+//     }
+
+//     // Update variables for the next layer
+//     row++;
+//     col++;
+//     increment += 2;
+//   }
+
+//   return matrix;
+// }
+
+// console.log(generateMatrix(1, 2));
+
+// function generateMatrix(n) {
+//   const matrix = [];
+//   for (let i = 0; i < n; i++) {
+//     matrix[i] = [];
+//     for (let j = 0; j < n; j++) {
+//       matrix[i][j] = 0; // Initialize all cells with 0
+//     }
+//   }
+
+//   let value = 1;
+//   let increment = 1;
+//   let row = 0;
+//   let col = 0;
+
+//   while (value <= n * n) {
+//     // Traverse right
+//     for (let i = col; i < n - col; i++) {
+//       matrix[row][i] = value;
+//       value++;
+//     }
+//     // Traverse down
+//     for (let i = row + 1; i < n - row; i++) {
+//       matrix[i][n - col - 1] = value;
+//       value++;
+//     }
+//     // Traverse left
+//     for (let i = n - col - 2; i >= col; i--) {
+//       matrix[n - row - 1][i] = value;
+//       value++;
+//     }
+//     // Traverse up
+//     for (let i = n - row - 2; i > row; i--) {
+//       matrix[i][col] = value;
+//       value++;
+//     }
+
+//     // Update variables for the next layer
+//     row++;
+//     col++;
+//     increment += 2;
+//   }
+
+//   return matrix;
+// }
+
+// console.log(generateMatrix(3));
+
+// ===========
+// =  ===    =
+// =  ===    =
+// ===========
+// 
+
+
+
+
+// function snakeXenzia(size) {
+
+//   const matrix = [];
+
+
+
+
+//   for (let i = 0; i < size; i++) {
+
+//       matrix[i] = [];
+
+//       for (let j = 0; j < size; j++) {
+
+//           matrix[i][j] = 0;
+
+//       }
+
+//   }
+
+
+
+
+//   console.log(matrix);
+
+
+
+
+//   let row = 0;
+
+//   let col = 0;
+
+//   let minRow = 0;
+
+//   let maxRow = size - 1;
+
+//   let minCol = 0;
+
+//   let maxCol = size - 1;
+
+//   let direction = 0;
+
+//   let counter = 1;
+
+
+
+
+//   while (counter <= size * size) {
+
+//       matrix[row][col] = counter;
+
+
+
+
+//       if (direction === 0) {
+
+//           if (col < maxCol) {
+
+//               col++;
+
+//           } else {
+
+//               direction = 1;
+
+//               minRow++;
+
+//               row++;
+
+//           }
+
+//       } else if (direction === 1) {
+
+//           if (row < maxRow) {
+
+//               row++;
+
+//           } else {
+
+//               direction = 2;
+
+//               maxCol--;
+
+//               col--;
+
+//           }
+
+//       } else if (direction === 2) {
+
+//           if (col > minCol) {
+
+//               col--;
+
+//           } else {
+
+//               direction = 3;
+
+//               maxRow--;
+
+//               row--;
+
+//           }
+
+//       } else if (direction === 3) {
+
+//           if (row > minRow) {
+
+//               row--;
+
+//           } else {
+
+//               direction = 0;
+
+//               minCol++;
+
+//               col++;
+
+//           }
+
+//       }
+
+
+
+
+//       counter++;
+
+//   }
+
+
+
+
+//   return matrix;
+
+// }
+
+
+function fibonacci(n){
+  // 0 and one are constants thus we create an array to them
+  const fib = [0,1] ;
+  for(let i= 2; i<=n;i++){
+      fib[i] = fib[i-1] + fib[i-2]
+  }
+  return fib
+}
+// O(n)
+console.log(fibonacci(3))
+console.log(fibonacci(9))
+console.log(fibonacci(5))
+
+
+function isPrime(n){
+  if(n<2){
+      return false
+  }
+  for (let i=2;i<n;i++){
+      if(n%i==0){
+          return false
+      }
+      return true
+  }
+}
+// O(n)
+console.log(isPrime(5));
+console.log(isPrime(4));
+console.log(isPrime(7));
